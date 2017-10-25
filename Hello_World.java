6749de96f8606aef5de1ccef2dbf3ad9b42620cd
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-25 12:20:41 -0500

    version 1.1

---
+++ Hello_World.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+	
+  public static void main(String[] args) {
+    System.out.println("Hola mundo");
+	Chicken Chicken;
+	Chicken=new Chicken();
+	Chicken.initializeData("lucy","brown",2);
+	Chicken.print();
+  }
+  
+}
+
