0478872491dd58fe7a4aa3620c4aa152b30c7df4
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-26 17:22:29 -0500

    second modification

--- Chicken.java
+++ Chicken.java
@@ -1,29 +1,28 @@
-
 public class Chicken{
-
 	private String name;
 	private String color;
 	private int age;
 	
-	
 		public void layAnEgg(){
-			System.out.println("The chicken lay an egg."); 
+			System.out.println("The chicken "+name+" laid an egg."); 
 		}
 		public void poop(){
-			System.out.println("The chicken pooped."); 
+			System.out.println("The chicken "+name+" did pooped."); 
 		}
 		public void drink(){
-			System.out.println("The chicken drank."); 
+			System.out.println("The chicken "+name+" drank water."); 
 		}
 		public void print(){	
-			System.out.println("The name of the chicken is: " + name + "color: " + color + "age: "+ age+ ""); 
+			System.out.println("The name of the chicken is:" + name + ", Color:" + color + "Age:"+ age+" "); 
 		}
 		
-	public void initializeData(String name, String color, int age){
-		this.name=name;
+	public void initializeData(String color, int age){
 		this.color=color;
 		this.age=age;
-		
+	}
+	
+	public void addName(String name){
+		this.name = name; 
 	}
 		
 }
