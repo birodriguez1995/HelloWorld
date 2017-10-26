ca992d6c969091d9312b679be6549f8643d8fe04
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-26 17:23:30 -0500

    Second modification

--- Hello_World.java
+++ Hello_World.java
@@ -1,11 +1,29 @@
-public class HelloWorld{
-	
+public class Hello_World{
   public static void main(String[] args) {
     System.out.println("Hola mundo");
-	Chicken Chicken;
-	Chicken=new Chicken();
-	Chicken.initializeData("lucy","brown",2);
-	Chicken.print();
+	Chicken chicken;
+	chicken=new Chicken();
+	chicken.initializeData("Brown, ", 2);
+	chicken.addName("Lucy");
+	chicken.print();
+	chicken.layAnEgg();
+	chicken.poop();
+	chicken.drink();
+	
+	Chicken grace;
+	grace=new Chicken();
+	grace.addName("Grace");
+	grace.layAnEgg();
+	
+	Chicken maid;
+	maid=new Chicken();
+	maid.addName("Maid");
+	maid.drink();
+	
+	Chicken sofia;
+	sofia=new Chicken();
+	sofia.addName("Sofia");
+	sofia.poop();
   }
   
 }
