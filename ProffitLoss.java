dce373cc1acf8881eda9373a40a5082c2b41c79a
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-29 19:39:18 -0500

    it was modified

---
+++ ProffitLoss.java
@@ -0,0 +1,19 @@
+public class ProffitLoss {
+
+	private float quantity;
+	private float salePrice;
+	private float investment;
+
+	/**
+	 * 
+	 * @param share
+	 * @param price
+	 * @param purchasePrice
+	 * @param purchaseTransference
+	 */
+	public void Pl(Share share, float price, float purchasePrice, double purchaseTransference) {
+		// TODO - implement ProffitLoss.Pl
+		throw new UnsupportedOperationException();
+	}
+
+}
