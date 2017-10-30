abc0f715ec1f6e7f81fe4e19b3c3647161f64ecc
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-29 19:37:02 -0500

    it was modified

---
+++ Analyst.java
@@ -0,0 +1,62 @@
+public class Analyst {
+
+	private float seedAmount;
+	private float transaction;
+	private String purchaseSaleHistory;
+	private int simulationDay;
+	private float totalProfitLoss;
+	private float stockProfitLossDay;
+	private float stockInvestmentDays;
+
+	/**
+	 * 
+	 * @param share
+	 * @param investment
+	 */
+	public void Inv(Share share, Investment investment) {
+		// TODO - implement Analyst.Inv
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param share
+	 * @param inv
+	 */
+	public void Pl(Share share, int inv) {
+		// TODO - implement Analyst.Pl
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param seedAmount
+	 * @param transaction
+	 * @param share
+	 */
+	public void Game(float seedAmount, Transaction transaction, Share share) {
+		// TODO - implement Analyst.Game
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param totalProfitLoss
+	 * @param day
+	 */
+	public void StockProfitLossPerDay(TotalProfitLoss totalProfitLoss, int day) {
+		// TODO - implement Analyst.StockProfitLossPerDay
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param saleTime
+	 * @param purchaseTime
+	 */
+	public void StockInvestmentDay(int saleTime, int purchaseTime) {
+		// TODO - implement Analyst.StockInvestmentDay
+		throw new UnsupportedOperationException();
+	}
+
+}
