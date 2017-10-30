d59d084e744db9bbf15b6fd8a32653b4fca11585
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-29 19:41:27 -0500

    it was modified

---
+++ Transaction.java
@@ -0,0 +1,17 @@
+public class Transaction {
+
+	private int type;
+	private float price;
+	private int saleTransfee;
+
+	/**
+	 * 
+	 * @param price
+	 * @param quantity
+	 */
+	public void buySellShare(float price, int quantity) {
+		// TODO - implement Transaction.buySellShare
+		throw new UnsupportedOperationException();
+	}
+
+}
