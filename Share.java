ce28600389627b536fe5dc5f8957f435c4b90ad9
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-29 19:40:27 -0500

    it was modified

---
+++ Share.java
@@ -0,0 +1,20 @@
+public class Share {
+
+	private float price;
+	private int quantity;
+	private float purchasePrice;
+	private float purchaseTransference;
+	private float salePrice;
+	private float profitLoss;
+
+	/**
+	 * 
+	 * @param broker
+	 * @param fee
+	 */
+	public void payFee(Broker broker, double fee) {
+		// TODO - implement Share.payFee
+		throw new UnsupportedOperationException();
+	}
+
+}
