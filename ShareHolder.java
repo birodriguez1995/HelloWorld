4b235499713a440cb8f42f1288a6cd43806631a8
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-29 19:40:49 -0500

    it was modified

---
+++ ShareHolder.java
@@ -0,0 +1,25 @@
+public class ShareHolder {
+
+	private float price;
+	private int shareNumber;
+	private float money;
+
+	/**
+	 * 
+	 * @param share
+	 */
+	public void buyShare(int share) {
+		// TODO - implement ShareHolder.buyShare
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param share
+	 */
+	public void sellShare(int share) {
+		// TODO - implement ShareHolder.sellShare
+		throw new UnsupportedOperationException();
+	}
+
+}
