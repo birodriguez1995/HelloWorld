96bcb5af58974254538564d3538f240048d57b0d
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-29 19:37:40 -0500

    it was modified

---
+++ Broker.java
@@ -0,0 +1,16 @@
+public class Broker {
+
+	private float fee;
+
+	/**
+	 * 
+	 * @param investor
+	 * @param analyst
+	 * @param fee
+	 */
+	public void gainFee(Investor investor, Analyst analyst, float fee) {
+		// TODO - implement Broker.gainFee
+		throw new UnsupportedOperationException();
+	}
+
+}
