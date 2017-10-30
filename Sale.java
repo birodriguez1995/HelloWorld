6887be95a7b54a05e1bdec60d9ed6489b1e50088
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-29 19:40:02 -0500

    it was modified

---
+++ Sale.java
@@ -0,0 +1,15 @@
+public class Sale {
+
+	private float price;
+
+	/**
+	 * 
+	 * @param share
+	 * @param salePrice
+	 */
+	public void sellShare(Share share, float salePrice) {
+		// TODO - implement Sale.sellShare
+		throw new UnsupportedOperationException();
+	}
+
+}
