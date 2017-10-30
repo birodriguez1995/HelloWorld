8951ec342f46fbc82395588162e26a5a4bcd64e3
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-29 19:37:25 -0500

    it was modified

---
+++ AnalystDecision.java
@@ -0,0 +1,14 @@
+public class AnalystDecision {
+
+	private int opinionAnalyst;
+
+	/**
+	 * 
+	 * @param opinionAnalyst
+	 */
+	public void response(int opinionAnalyst) {
+		// TODO - implement AnalystDecision.response
+		throw new UnsupportedOperationException();
+	}
+
+}
