4d5d04176da4435bc1e960cf598a801b401e45ed
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-29 19:38:40 -0500

    it was modified

---
+++ Investment.java
@@ -0,0 +1,20 @@
+public class Investment {
+
+	private float quantity;
+	private float purchasePrice;
+	private double payFee;
+	private double saleTransferenceFee;
+
+	/**
+	 * 
+	 * @param analyst
+	 * @param share
+	 * @param purchasePrice
+	 * @param salePrice
+	 */
+	public void ProfitLoss(Analyst analyst, Share share, double purchasePrice, double salePrice) {
+		// TODO - implement Investment.ProfitLoss
+		throw new UnsupportedOperationException();
+	}
+
+}
