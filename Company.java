908555e120ddb6962b855058b174a275763ddddb
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-29 19:38:15 -0500

    it was modified

---
+++ Company.java
@@ -0,0 +1,30 @@
+public class Company {
+
+	private String symbol;
+	private int price;
+	private int share;
+	private int companyValue;
+	private int shareholder;
+	private int investor;
+
+	/**
+	 * 
+	 * @param shareHolder
+	 * @param share
+	 */
+	public void sellShare(ShareHolder shareHolder, int share) {
+		// TODO - implement Company.sellShare
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param investor
+	 * @param companyValue
+	 */
+	public void acquireInvestor(Investor investor, int companyValue) {
+		// TODO - implement Company.acquireInvestor
+		throw new UnsupportedOperationException();
+	}
+
+}
