2f17e84e55bddef628eaa3615e33df85e012a574
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-31 23:22:09 -0500

    Final Version

---
+++ Invstment.java
@@ -0,0 +1,14 @@
+public class Invstment {
+
+	private float quantity;
+	private double purchaseTransferenceFee;
+	private double saleTransferenceFee;
+	private floar purchasePrice;
+	private Investor[] investor;
+
+	public void profitLoss() {
+		// TODO - implement Investment.profitLoss
+		throw new UnsupportedOperationException();
+	}
+
+}
