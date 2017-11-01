22a1b451b825ca50574bd79c0a0d8c87338b66b6
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-31 23:13:47 -0500

    Final Version

---
+++ Comparer.java
@@ -0,0 +1,22 @@
+public class Comparer {
+
+	private Analyst[] listOfAnalyst;
+	private String outputFile;
+	private int analystCount;
+	private String[] symbols;
+
+	/**
+	 * 
+	 * @param msg
+	 */
+	public void load(String[] msg) {
+		// TODO - implement Comparer.load
+		throw new UnsupportedOperationException();
+	}
+
+	public void compare() {
+		// TODO - implement Comparer.compare
+		throw new UnsupportedOperationException();
+	}
+
+}
