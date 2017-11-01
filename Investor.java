a225f9844b868b0d5b690a45e26e481757548dd7
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-31 23:16:00 -0500

    Final version

--- Investor.java
+++ Investor.java
@@ -1,23 +1,13 @@
 public class Investor {
 
-	private String analystHistory;
+	private int saleTime;
+	private String broker;
+	private int quantityShare;
 	private double payFee;
 	private int purchaseTime;
-	private int saleTime;
-	private int quantityShare;
-	private String broker;
 
-	/**
-	 * 
-	 * @param share
-	 * @param quantity
-	 * @param broker
-	 * @param salePrice
-	 * @param analyst
-	 * @param inv
-	 */
-	public void TotalProfitLoss(Share share, Quantity quantity, Broker broker, float salePrice, Analyst analyst, Inv inv) {
-		// TODO - implement Investor.TotalProfitLoss
+	public void totalProfitLoss() {
+		// TODO - implement Investor.totalProfitLoss
 		throw new UnsupportedOperationException();
 	}
 
