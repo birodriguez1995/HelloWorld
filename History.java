b2c253bbb8151e0ccb22270cd9f04aa56c5a94c6
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-31 23:14:25 -0500

    Final Version

---
+++ History.java
@@ -0,0 +1,39 @@
+public class History {
+
+	private int currentPurchaseSale;
+	private int seedMoney;
+	private int simulationDay;
+	private int purchaseSaleCount;
+	private PurchaseSale[] purchaseSales;
+
+	public int initialMoney() {
+		// TODO - implement History.initialMoney
+		throw new UnsupportedOperationException();
+	}
+
+	public int simulationDay() {
+		// TODO - implement History.simulationDay
+		throw new UnsupportedOperationException();
+	}
+
+	public void restartIteration() {
+		// TODO - implement History.restartIteration
+		throw new UnsupportedOperationException();
+	}
+
+	public int totalProfitLoss() {
+		// TODO - implement History.totalProfitLoss
+		throw new UnsupportedOperationException();
+	}
+
+	public int profitLossPerDay() {
+		// TODO - implement History.profitLossPerDay
+		throw new UnsupportedOperationException();
+	}
+
+	public PurchaseSale nextPurchaseSale() {
+		// TODO - implement History.nextPurchaseSale
+		throw new UnsupportedOperationException();
+	}
+
+}
