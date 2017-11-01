6aa1c8eacb128d59fc9256fe0f0732eb1cedcf61
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-10-31 23:16:28 -0500

    Final Version

---
+++ PurchaseSale.java
@@ -0,0 +1,43 @@
+public class PurchaseSale {
+
+	private String symbol;
+	private int quatity;
+	private int purchaseDateTime;
+	private int purchasePrice;
+	private Investment[] investment;
+	private int saleFee;
+	private int salePrice;
+	private int saleDateTime;
+	private int purchaseFee;
+
+	public int purchaseDateTime() {
+		// TODO - implement PurchaseSale.purchaseDateTime
+		throw new UnsupportedOperationException();
+	}
+
+	public int saleDateTime() {
+		// TODO - implement PurchaseSale.saleDateTime
+		throw new UnsupportedOperationException();
+	}
+
+	public int investmentAmount() {
+		// TODO - implement PurchaseSale.investmentAmount
+		throw new UnsupportedOperationException();
+	}
+
+	public int profitLoss() {
+		// TODO - implement PurchaseSale.profitLoss
+		throw new UnsupportedOperationException();
+	}
+
+	public String symbol() {
+		// TODO - implement PurchaseSale.symbol
+		throw new UnsupportedOperationException();
+	}
+
+	public void purchaseSale() {
+		// TODO - implement PurchaseSale.purchaseSale
+		throw new UnsupportedOperationException();
+	}
+
+}
