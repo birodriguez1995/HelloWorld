5467a96a33316d79f3757f01526d6ccd78d7e0a1
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-11-05 18:13:38 -0500

    Version 2.0

--- Chicken.java
+++ Chicken.java
@@ -24,6 +24,38 @@ public class Chicken{
 	public void addName(String name){
 		this.name = name; 
 	}
-		
+	//constructor
+	public Chicken(String name,String color,int age){
+		this.name=name;
+		this.color=color;
+		this.age=age;
+	}
+	
+	//setters
+	public void setName(String name){
+		this.name=name;		
+	}
+	
+	public void setColor(String color){
+		this.color=color;		
+	}
+	
+	public void setAge(int age){
+		this.age=age;		
+	}
+	//getters
+	public String getName(){
+		return this.name;		
+	}
+	
+	public String getColor(){
+		return this.color;		
+	}
+	
+	public int getAge(){
+		return this.age;		
+	}
 }
+	
+		
 
