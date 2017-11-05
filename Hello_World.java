6ee85e9f79f1a5effd0ffe7aa57b915a3b4f1c94
birodriguez1995 <bryanrodriguez14@hotmail.com>
2017-11-05 18:14:05 -0500

    Version 2.0

--- Hello_World.java
+++ Hello_World.java
@@ -2,7 +2,7 @@ public class Hello_World{
   public static void main(String[] args) {
     System.out.println("Hola mundo");
 	Chicken chicken;
-	chicken=new Chicken();
+	chicken=new Chicken("Lucy","Brown", 2);
 	chicken.initializeData("Brown, ", 2);
 	chicken.addName("Lucy");
 	chicken.print();
@@ -11,20 +11,24 @@ public class Hello_World{
 	chicken.drink();
 	
 	Chicken grace;
-	grace=new Chicken();
+	grace=new Chicken("Grace","red",5);
 	grace.addName("Grace");
 	grace.layAnEgg();
 	
 	Chicken maid;
-	maid=new Chicken();
+	maid=new Chicken("Maid","white",6);
 	maid.addName("Maid");
 	maid.drink();
 	
 	Chicken sofia;
-	sofia=new Chicken();
+	sofia=new Chicken("sofia","black",3);
 	sofia.addName("Sofia");
-	sofia.poop();
+	sofia.setName("Anita");
+	System.out.println("El nombre de la gallina es: "+sofia.getName()+" ");
+	sofia.setColor("Cafe");
+	System.out.println("El color de la gallina es: "+sofia.getColor()+" ");
+	sofia.setAge(10);
+	System.out.println("La edad de la gallina es: "+sofia.getAge()+" ");
   }
-  
 }
 
